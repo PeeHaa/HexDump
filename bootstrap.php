dd96e0c891ec0d1a952779a99e56f552e65e39af
PeeHaa <info@pieterhordijk.com>
2013-07-07 00:36:21 +0200

    Added error reporting and timezone setting to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -16,6 +16,18 @@ use HexDump\Core\Autoloader,
     HexDump\Http\Request;
 
 /**
+ * Setup error reporting
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+
+/**
+ * Setup timezone
+ */
+ini_set('date.timezone', 'Europe/Amsterdam');
+
+/**
  * Bootstrap the PitchBlade library
  */
 require_once __DIR__ . '/src/HexDump/bootstrap.php';
